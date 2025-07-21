132a86c652922d5b26bbd96165fa664f6042db09
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2025-07-21 21:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const div = document.getElementById("counter");
+const btn = document.querySelector('button');
+let number = 0;
+btn.addEventListener('click',function(e){
+	alert(number);
+	number++;
+
+	div.innerText = number;
+});
